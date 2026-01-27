b41633e6f879b39b4a64ef8095f8e8b7ac6073c5
Michael Tyler <mktyler01@gmail.com>
2026-01-27 11:39:57 -0600

    Adding initialize header and search box styles

--- goodreads-search/src/components/header/SearchBoxHeader.tsx
+++ goodreads-search/src/components/header/SearchBoxHeader.tsx
@@ -4,6 +4,9 @@ import "./SearchBoxHeader.css";
 export default function SearchBoxHeader() {
     return (
         <div className="search-box-header">
+            <h1 className="search-box-header-title">
+                Discover your next good read.
+            </h1>
             <SearchBoxInput />
         </div>
     )
--- goodreads-search/src/components/header/SearchBoxInput.tsx
+++ goodreads-search/src/components/header/SearchBoxInput.tsx
@@ -1,7 +1,16 @@
 import { SearchBox } from "react-instantsearch";
+import "./SearchBoxInput.css";
 
 export default function SearchBoxInput() {
     return (
-        <SearchBox />
+        <SearchBox
+            classNames={{
+                form: "search-box-form",
+                input: "search-box-input",
+                submit: "search-box-submit-btn",
+                reset: "search-box-reset-btn"
+            }}
+            placeholder="Search books by title, author, genre, or description"
+        />
     )
 }
