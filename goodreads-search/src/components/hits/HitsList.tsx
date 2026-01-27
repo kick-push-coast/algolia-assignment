7f26c52b27d03328d5929cfec5f644dd404eabc4
Michael Tyler <mktyler01@gmail.com>
2026-01-27 12:31:51 -0600

    Adding sort widget

--- goodreads-search/src/components/hits/HitsList.tsx
+++ goodreads-search/src/components/hits/HitsList.tsx
@@ -1,14 +1,28 @@
 import { BookHit } from "./BookHit";
-import { Hits } from "react-instantsearch";
+import { Hits, SortBy, Stats } from "react-instantsearch";
 import "./HitsList.css";
 
 export default function HitsList() {
     return (
-        <Hits
-            hitComponent={BookHit} 
-            classNames={{
-                list: "hits-list"
-            }}
-        />
+        <div className="hits-list-container">
+            <div className="hits-list-header">
+                <Stats />
+                <SortBy
+                    items={[
+                        { label: "Sort by relevance", value: "goodreads-popular-clean" },
+                        { label: "Sort by rating", value: "goodreads-popular-clean_rating_desc" }
+                    ]}
+                    classNames={{
+                        select: "hits-list-sort-select"
+                    }}
+                />
+            </div>
+            <Hits
+                hitComponent={BookHit} 
+                classNames={{
+                    list: "hits-list"
+                }}
+            />
+        </div>
     )
 }
