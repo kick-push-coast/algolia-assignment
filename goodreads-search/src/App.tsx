53c699b1dcecceb06afa499727849d4f1ffb726e
Michael Tyler <mktyler01@gmail.com>
2026-01-27 13:33:34 -0600

    Implementing genres refinement widget

--- goodreads-search/src/App.tsx
+++ goodreads-search/src/App.tsx
@@ -1,7 +1,8 @@
 import { liteClient as algoliasearch } from "algoliasearch/lite";
-import { InstantSearch } from "react-instantsearch";
+import { InstantSearch, RefinementList } from "react-instantsearch";
 import SearchBoxHeader from "./components/header/SearchBoxHeader";
 import HitsList from "./components/hits/HitsList";
+import "./App.css";
 
 const searchClient = algoliasearch("LLK6VK0IIY", "1601f62f4e3e133f6fbc2b67e744afc9");
 
@@ -9,7 +10,26 @@ export default function App() {
 	return (
 		<InstantSearch searchClient={searchClient} indexName="goodreads-popular-clean">
 			<SearchBoxHeader />
-			<HitsList />
+			<div className="layout-container">
+				<div className="layout-refinements">
+					<h3 className="refinements-genre-header">Genres</h3>
+					<RefinementList 
+						attribute="genres"
+						limit={8}
+						showMore
+						classNames={{
+							list: "refinements-genre-list",
+							item: "refinements-genre-item",
+							labelText: "refinements-genre-label",
+							count: "refinements-genre-count",
+							showMore: "refinements-genre-show-more"
+						}}
+					/>
+				</div>
+				<div className="layout-hits">
+					<HitsList />
+				</div>
+			</div>
 		</InstantSearch>
 	);
 }
